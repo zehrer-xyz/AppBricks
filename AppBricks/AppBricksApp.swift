fb810bc7e31f34f1059164a967fa85d554d9f71b
stephan-xyz <stephan@zehrer.xyz>
2026-01-25 21:15:26 +0100

    Initial Commit

---
+++ AppBricks/AppBricksApp.swift
@@ -0,0 +1,17 @@
+//
+//  AppBricksApp.swift
+//  AppBricks
+//
+//  Created by Stephan Zehrer on 25.01.26.
+//
+
+import SwiftUI
+
+@main
+struct AppBricksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ AppBricksTests/AppBricksTests.swift
@@ -0,0 +1,17 @@
+//
+//  AppBricksTests.swift
+//  AppBricksTests
+//
+//  Created by Stephan Zehrer on 25.01.26.
+//
+
+import Testing
+@testable import AppBricks
+
+struct AppBricksTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}

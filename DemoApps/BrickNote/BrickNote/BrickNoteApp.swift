b5a35fbe17dd7038168d9fedb205462926f19122
stephan-xyz <stephan@zehrer.xyz>
2026-02-03 08:03:21 +0100

    add BrickNoteApp

---
+++ DemoApps/BrickNote/BrickNote/BrickNoteApp.swift
@@ -0,0 +1,27 @@
+//
+//  Copyright (c) 2026 zehrer.xyz
+//  Author: Stephan Zehrer
+//
+//  This file is part of AppBricks.
+//
+//  AppBricks is dual-licensed:
+//  - GPLv3 for open-source use
+//  - Commercial license required for proprietary use
+//
+// SPDX-License-Identifier: GPL-3.0-only
+//
+//  See the LICENSE file for details.
+//
+//  BrickNoteApp.swift
+//
+
+import SwiftUI
+
+@main
+struct BrickNoteApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ DemoApps/BrickNote/BrickNote/ContentView.swift
@@ -0,0 +1,34 @@
+//
+//  Copyright (c) 2026 zehrer.xyz
+//  Author: Stephan Zehrer
+//
+//  This file is part of AppBricks.
+//
+//  AppBricks is dual-licensed:
+//  - GPLv3 for open-source use
+//  - Commercial license required for proprietary use
+//
+// SPDX-License-Identifier: GPL-3.0-only
+//
+//  See the LICENSE file for details.
+//
+//  ContentView.swift
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
